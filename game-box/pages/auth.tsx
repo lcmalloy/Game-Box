54a102a5253b8670e514b91fe740d856e2797cd1
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-22 15:09:53 -0700

    added Google and Github Icons to Auth page

--- game-box/pages/auth.tsx
+++ game-box/pages/auth.tsx
@@ -3,6 +3,8 @@ import React, { useState, useCallback } from 'react'
 import axios from 'axios'
 import { signIn } from 'next-auth/react'
 import { useRouter } from "next/router";
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 const Auth = () => {
   const [email, setEmail] = useState("")
@@ -25,7 +27,6 @@ const Auth = () => {
         callbackUrl: '/'
       });
       router.push('/')
-      
     } catch(error) {
       console.log(error)
     }
@@ -38,13 +39,11 @@ const Auth = () => {
         name,
         password,
       })
-
       login();
     } catch(error) {
       console.log(error)
     }
   }, [email, name, password, login])
-
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero2.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -85,6 +84,14 @@ const Auth = () => {
               >
                 {variant === 'login' ? 'Login' : 'Sign Up'}
               </button>
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FcGoogle size={30}/>
+                </div>
+                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FaGithub size={30}/>
+                </div>
+              </div>
               <p className="text-neutral-500 mt-12">
                 {variant === 'login' ? 'First time using GameBox?' : 'Already have an account?'}
                 <span className="text-white ml-3 hover:underline cursor-pointer" onClick={toggleVariant}>
