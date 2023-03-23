4f984a28df284a4b0c2e42d6177ccbeef32ed01c
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-22 17:00:09 -0700

    added serverAuth and Route protection

--- game-box/pages/index.tsx
+++ game-box/pages/index.tsx
@@ -1,7 +1,34 @@
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
+
+import useCurrentUser from '../hooks/useCurrentUser'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return  {
+    props: {}
+  }
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1>Game Box</h1>
+      {console.log(user)}
+      <h1 className="text-4xl text-green-400">Game Box</h1>
+      <p className="text-white">Logged in as:  {user?.email}</p>
+      <button className="h-10 w-20 bg-white" onClick={() => signOut()}>Logout</button>
     </>
   )
 }
