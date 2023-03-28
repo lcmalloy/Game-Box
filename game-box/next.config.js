e503400502b7c7ffd4ae94dceb1ce67f7719b830
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-27 17:43:25 -0700

    updated next.config files

--- game-box/next.config.js
+++ game-box/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/Game-Box',
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
